39a63b5f8b2f610319c62bee1c36fab78eae9b6f
ketigabunia15-hash <ketigabunia15@gmail.com>
2026-01-08 01:54:53 +0400

    footer.module.css

--- midterm-exam---react.js/src/app/layout.js
+++ midterm-exam---react.js/src/app/layout.js
@@ -25,3 +25,22 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
+importy './globals.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+export const metadata={
+  title:'Midterm Project',
+};
+
+export default function RootLayout({children}){
+  return (
+    <html lang="en">
+      <body>
+        <NavBar/>
+        <main>{children}</main>
+        <Footer/>   
+      </body>
+    </html>
+  );
+}
