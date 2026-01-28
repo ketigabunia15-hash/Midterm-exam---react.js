6a514104de7d31ddfaf37785360987de095dda8b
ketigabunia15-hash <ketigabunia15@gmail.com>
2026-01-29 00:38:53 +0400

    view details

--- midterm-exam---react.js/src/app/products/details/[id]/page.js
+++ midterm-exam---react.js/src/app/products/details/[id]/page.js
@@ -2,10 +2,13 @@
 
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../../redux/cartSlice';
 import styles from './details.module.css';
 
 export default function ProductDetails() {
   const params = useParams();
+  const dispatch = useDispatch();
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
@@ -19,17 +22,31 @@ export default function ProductDetails() {
 
   if (!product) return <p>Loading...</p>;
 
+  const stars = Math.round(product.rating?.rate || 0);
+
   return (
     <div className={styles.details}>
-      <img src={product.image} alt={product.title} />
+      <img src={product.image} alt={product.title} className={styles.image} />
 
       <div className={styles.info}>
         <h1>{product.title}</h1>
         <p>{product.description}</p>
         <p><b>Category:</b> {product.category}</p>
         <p><b>Price:</b> ${product.price}</p>
-        <p><b>Rating:</b> {product.rating?.rate}</p>
-        <p><b>Reviews:</b> {product.rating?.count}</p>
+
+        <p>
+          <b>Rating:</b>{' '}
+          {Array.from({ length: stars }, (_, i) => '⭐').join('')}
+          {' '}
+          ({product.rating?.count} reviews)
+        </p>
+
+        <button
+          className={styles.addButton}
+          onClick={() => dispatch(addToCart(product))}
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   );
